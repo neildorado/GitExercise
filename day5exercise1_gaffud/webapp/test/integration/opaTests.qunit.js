dc3fdd92af78df0934cfc0d9446835463ce745cb
Neil Dorado <neil.d.dorado@accenture.com>
2023-03-22 07:40:25 +0000

    remove

--- day5exercise1_gaffud/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["zbtp/day5exercise1gaffud/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- day5exercise1_gaffud/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"zbtp/day5exercise1_gaffud/test/unit/controller/Home.controller"
-], function () {
-	"use strict";
-});
